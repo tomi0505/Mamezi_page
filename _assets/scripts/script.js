df129c4b5b743dfe549aa31787fc889cd406da71
id github: tomi0505 <tomaszgrzyb.online@gmail.com>
2018-09-12 18:52:16 +0200

    _

--- _assets/scripts/script.js
+++ _assets/scripts/script.js
@@ -1,19 +1,21 @@
+"use strict";
 
-let productBox = 1;
-let refreshProductsObject = "";
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
+var productBox = 1;
+var refreshProductsObject = "";
 
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", function () {
 
   // AJAX
-  const url = "http://tomaszgrzyb.eu/projects/Mamezi_page/_assets/scripts/products.json";
+  var url = "http://tomaszgrzyb.eu/projects/Mamezi_page/_assets/scripts/products.json";
 
-  const request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
   request.open("GET", url);
 
   request.onload = function () {
     if (request.status === 200) {
-      const products = JSON.parse(request.responseText);
+      var products = JSON.parse(request.responseText);
       refreshProductsObject = products;
       updateElements(products);
     }
@@ -21,86 +23,126 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
   request.send(null);
 
-  
   // SHOW_PRODUCTS
-  const productsCounter = document.getElementById("productsCounter");
+  var productsCounter = document.getElementById("productsCounter");
 
-  for (let i = 0; i < (productsCounter.value - productBox); i++) {
+  for (var i = 0; i < productsCounter.value - productBox; i++) {
     createElements();
   }
 
-  productsCounter.addEventListener("change", (e)=> {
+  productsCounter.addEventListener("change", function (e) {
     removeElements();
-    for (let i = 0; i < (e.target.value - productBox); i++) {
+    for (var _i = 0; _i < e.target.value - productBox; _i++) {
       createElements();
     }
     updateElements(refreshProductsObject);
   }, false);
-
 }, false);
-
 
 // CREATE_ELEMENTS
 function createElements() {
 
-  const products = document.querySelector(".products");
-  const productBox = document.querySelector(".productBox");
-  const codeHTML = productBox.innerHTML;
+  var products = document.querySelector(".products");
+  var productBox = document.querySelector(".productBox");
+  var codeHTML = productBox.innerHTML;
 
   // CREATE_NEW_ELEMENT
-  const productBoxNew = document.createElement("section");
+  var productBoxNew = document.createElement("section");
   productBoxNew.classList.add("productBox");
   productBoxNew.innerHTML = codeHTML;
 
   products.appendChild(productBoxNew);
-
 }
-
 
 // REMOVE_ELEMENTS
 function removeElements() {
-  const products = document.querySelector(".products");
-  let productBox = document.querySelectorAll(".productBox");
-  productBox = [...productBox];
-  productBox.forEach((items, index) => {
-    if(index > 0) {
+  var products = document.querySelector(".products");
+  var productBox = document.querySelectorAll(".productBox");
+  productBox = [].concat(_toConsumableArray(productBox));
+  productBox.forEach(function (items, index) {
+    if (index > 0) {
       products.removeChild(items);
     }
   });
 }
 
-
 // UPDATE_ELEMENTS
 function updateElements(products) {
 
-  let product = document.querySelectorAll(".product");
-  product = [...product];
+  var product = document.querySelectorAll(".products .product");
+  var productTitle = document.querySelectorAll(".products .product .title");
+  var price = document.querySelectorAll(".products .product .price");
+  var oldPrice = document.querySelectorAll(".products .product .oldPrice");
+  var company = document.querySelectorAll(".products .product .company");
 
-  product.forEach((items, index)=> {
+  product = [].concat(_toConsumableArray(product));
+  productTitle = [].concat(_toConsumableArray(productTitle));
+  price = [].concat(_toConsumableArray(price));
+  oldPrice = [].concat(_toConsumableArray(oldPrice));
+  company = [].concat(_toConsumableArray(company));
+
+  // PRODUCT
+  product.forEach(function (items, index) {
 
     if (index < products.count) {
 
       items.removeChild(items.firstChild);
 
-      const urlNameImage = products.list[index].main_image;
-      const urlImage = `https://www.outletmeblowy.pl/environment/cache/images/300_300_productGfx_${urlNameImage}.jpg`;
+      var urlNameImage = products.list[index].main_image;
+      var urlImage = "https://www.outletmeblowy.pl/environment/cache/images/300_300_productGfx_" + urlNameImage + ".jpg";
 
-      const image = new Image();
+      var image = new Image();
       image.src = urlImage;
 
       items.prepend(image);
-
     } else {
 
       items.removeChild(items.firstChild);
 
-      const image = new Image();
-      image.src = `http://tomaszgrzyb.eu/projects/Mamezi_page/_assets/img/empty_banner.png`;
+      var _image = new Image();
+      _image.src = "http://tomaszgrzyb.eu/projects/Mamezi_page/_assets/img/empty_banner.png";
 
-      items.prepend(image);
-
+      items.prepend(_image);
     }
-
   });
 
+  // PRODUCT_TITLE
+  productTitle.forEach(function (items, index) {
+
+    if (index < products.count) {
+      items.innerHTML = products.list[index].name;
+    } else {
+      items.innerHTML = "Twoja reklama";
+    }
+  });
+
+  // PRICE
+  price.forEach(function (items, index) {
+
+    if (index < products.count) {
+      items.innerHTML = products.list[index].price.gross.base;
+    } else {
+      items.innerHTML = "50 zł/msc.";
+    }
+  });
+
+  // OLD_PRICE
+  oldPrice.forEach(function (items, index) {
+
+    if (index < products.count) {
+      items.innerHTML = products.list[index].price.gross.promo;
+    } else {
+      items.innerHTML = "150 zł/msc.";
+    }
+  });
+
+  // COMPANY
+  company.forEach(function (items, index) {
+
+    if (index < products.count) {
+      items.innerHTML = products.list[index].producer.name;
+    } else {
+      items.innerHTML = "Promocja!";
+    }
+  });
 }
